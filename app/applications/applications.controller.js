c31df0feb0a86c2090cc5ce2ba935ffbe356a258
scorpion-jsb <tinyscorpion1101@gmail.com>
2015-06-22 03:05:27 -0400

    Deleting now has a confirm dialog and request uses name instead of id. Applications add function added.

--- app/applications/applications.controller.js
+++ app/applications/applications.controller.js
@@ -24,16 +24,19 @@ angular.module('hypercube')
 				$scope.data.error = err;
 			});
 		};
-		$scope.delete = function(ind){
+		$scope.delete = function(ind, ev){
 			$scope.data.loading = true;
-			var applicationName = $scope.applications[ind].name;
-			applicationsService.del(applicationName).then(function(response){
-				$scope.applications.splice(ind, 1);
-				$log.info('application deleted successfully');
-			}, function(err){
-				$log.error('Error loading applications', err);
-				$scope.data.loading = false;
-				$scope.data.error = err;
+			var application = $scope.applications[ind];
+			$scope.showConfirm(ev, {title:"Delete", description:"Are you sure you want to delete " + application.name + " ?"}).then(function(){
+				$log.log('calling delete with id:', application._id);
+				applicationsService.del(application.name).then(function(deletedApp){
+					$log.log('application deleted successfully', deletedApp);
+					$scope.applications.splice(ind, 1);
+				}, function(err){
+					$log.error('Error loading applications', err);
+					$scope.data.loading = false;
+					$scope.data.error = err;
+				});
 			});
 		};
 }])

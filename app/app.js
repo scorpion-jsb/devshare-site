5af260992cba9e5334db5d458666af64c32b82b0
scorpion-jsb <tinyscorpion1101@gmail.com>
2015-07-16 15:50:29 -0400

    and  decorators added to only show logs in local dev mode, and send to trackjs in production.

--- app/app.js
+++ app/app.js
@@ -29,11 +29,53 @@ angular.module('hypercube', [
   };
  })
 //Set environment based on host
-.service('ENV', ['$location', 'CONST', function($location, CONST){
+.service('ENV', ['$location', 'CONST', function ($location, CONST){
   //TODO: Check for other environments as well (staging)
   if($location.host() == "localhost"){
-    return {serverUrl:CONST.local.SERVER_URL, fbUrl:CONST.local.FB_URL};
+    return {serverUrl:CONST.local.SERVER_URL, fbUrl:CONST.local.FB_URL, logging:true};
   } else {
-    return {serverUrl:CONST.production.SERVER_URL, fbUrl:CONST.production.FB_URL};
+    return {serverUrl:CONST.production.SERVER_URL, fbUrl:CONST.production.FB_URL, logging:false};
   }
+}])
+.config(["$provide", function ($provide) {
+  $provide.decorator('$log', ['$delegate', '$window', function ($delegate, $window){
+    // Keep track of the original debug method, we'll need it later.
+    var origDebug = $delegate.debug;
+    var origLog = $delegate.log;
+    /*
+     * Intercept the call to $log.debug() so we can add on 
+     * our enhancement. We're going to add on a date and 
+     * time stamp to the message that will be logged.
+     */
+     //Not local development
+     if($window.location.host.split(":")[0] != "localhost"){
+      $delegate.log = angular.noop;
+      $delegate.info = angular.noop;
+      $delegate.debug = angular.noop;
+      $delegate.error = angular.noop;
+     } else {
+      $delegate.debug = function () {
+        var args = [].slice.call(arguments);
+        args[0] = [new Date().toString(), ': ', args[0]].join('');
+        
+        // Send on our enhanced message to the original debug method.
+        origDebug.apply(null, args);
+      };
+     }
+    return $delegate;
+  }])
+  $provide.decorator('$exceptionHandler', ["$delegate", "$window", function ($delegate, $window) {
+    return function (exception, cause) {
+      console.log('$window.location:', $window.location.host);
+      if($window.location.host.split(":")[0] != "localhost") {
+        if($window.trackJs){
+          $window.trackJs.track(exception);
+        }
+      }
+      //Running locally
+      else { 
+        $delegate(exception, cause);
+      } 
+    }; 
+  }]); 
 }]);

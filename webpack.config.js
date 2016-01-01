ffce4417499c3a16bccf86fa5ea6b0ae05aa1887
scorpion-jsb <tinyscorpion1101@gmail.com>
2016-01-01 16:14:46 -0500

    webpack with babel loader added

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var webpack = require("webpack")
+
+module.exports = {
+  entry: './app.js'
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+
+    module: {
+        loaders: [
+          {
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loaders: ['babel']
+          }
+
+        ]
+    }
+};

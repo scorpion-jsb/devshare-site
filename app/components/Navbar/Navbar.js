25a18015de5ff4b5c787753fd6d6c8417f1e0af9
scorpion-jsb <tinyscorpion1101@gmail.com>
2016-01-01 19:38:15 -0500

    Added Material UI appbar

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -1,8 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 
-import AccountManager from '../AccountManager/AccountManager';
 import './Navbar.scss';
+//React Components
+import AppBar from 'material-ui/lib/app-bar';
+import IconButton from 'material-ui/lib/icon-button';
+import NavigationClose from 'material-ui/lib/svg-icons/navigation/close';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import MenuItem from 'material-ui/lib/menus/menu-item';
 
 class Navbar extends Component {
   constructor(props) {
@@ -15,17 +21,24 @@ class Navbar extends Component {
   render() {
     let brandLinkLoc = (this.props.account && this.props.account.username) ? '/account' : '/';
     let brandLink = <Link to={ brandLinkLoc }>Hypercube</Link>
+    let iconButton = (<IconButton><MoreVertIcon /></IconButton>);
     return (
-      <div className="Navbar">
-        <div className="Navbar-Brand">
-          { brandLink }
-        </div>
-        <div className="Navbar-Spacer"></div>
-        <AccountManager
-          account={ this.props.account }
-          onLogoutClick={ this.props.onLogoutClick }
-        />
-      </div>
+      <AppBar
+        title="Hypercube"
+        iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+        iconElementRight={
+          <IconMenu
+            iconButtonElement={ iconButton }
+            targetOrigin={{horizontal: 'right', vertical: 'top'}}
+            anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+          >
+            <MenuItem primaryText="Refresh" />
+            <MenuItem primaryText="Help" />
+            <MenuItem primaryText="Sign out" />
+          </IconMenu>
+        }
+      />
+
     )
   }
 }

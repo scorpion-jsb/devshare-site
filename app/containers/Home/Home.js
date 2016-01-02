5d06e00b22611cf434465d03079808acfefda67b
scorpion-jsb <tinyscorpion1101@gmail.com>
2016-01-02 01:43:44 -0500

    added description to home page

--- app/containers/Home/Home.js
+++ app/containers/Home/Home.js
@@ -9,6 +9,14 @@ export default class Home extends Component {
     return (
       <div className="Home">
         <h2>Hypercube</h2>
+        <p>a web-based collaborataive code editor & project building environment</p>
+        <h3>Hypercube is built using the following frameworks & libraries:</h3>
+        <ul>
+          <li>React - View Rendering</li>
+          <li>Redux - State management</li>
+          <li>Matter - Authentication through Tessellate</li>
+          <li>Grout - Application building/management communication with Tessellate</li>
+        </ul>
 
         <a href="https://github.com/prescottprue/hypercube">
           <RaisedButton label="View code on GitHub" />

0e25bc31bf7243ce953c5432e0b4292e5c46bc2e
scorpion-jsb <tinyscorpion1101@gmail.com>
2016-01-04 06:34:33 -0500

    Production build now running.

--- bin/server.js
+++ bin/server.js
@@ -3,16 +3,27 @@
 var Express = require('express');
 var fs = require('fs');
 var path = require('path');
+var serveStatic = require('serve-static');
 
 var config = require('../webpack-production.config');
 
 var renderIndex = require('../lib/render-index');
 var renderApp = require('../build/bundle-server');
 
-var serveStatic = require('serve-static');
+try {
+  var stats = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'stats.json')));
+} catch(err) {
+  console.log('Error parsing stats file:', err);
+}
 
 var app = new Express();
 
+app.use('/' + config.publicPath, serveStatic('assets', {
+  index: false,
+  setHeaders: function(res) {
+    res.setHeader('Cache-Control', 'max-age=2592000');
+  }
+}));
 app.use('/' + config.publicPath, serveStatic(config.output.path, {
   index: false,
   setHeaders: function(res) {
@@ -23,22 +34,25 @@ app.use('/' + config.publicPath, serveStatic(config.output.path, {
 // This is fired every time the server side receives a request
 app.use(handleRender);
 
-var stats = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'stats.json')));
 
 function handleRender(req, res) {
-  renderApp(function (result) {
-    res.send(renderIndex({
-      hash: stats.hash,
-      appData: result.appData,
-      appMarkup: result.appMarkup
-    }));
-  });
+  if (typeof renderApp === 'function') {
+    renderApp(function (result) {
+      res.send(renderIndex({
+        hash: stats.hash,
+        appData: result.appData,
+        appMarkup: result.appMarkup
+      }));
+    });
+  } else {
+    console.log('Handle render tried to run and is not a function');
+  }
 }
 
 app.listen(config.port, function(error) {
   if (error) {
     console.error(error);
   } else {
-    console.info(`==> 🌎  Listening on port ${config.port}. Open up http://localhost:${config.port}/ in your browser.`);
+    console.info(`Listening on port ${config.port}`);
   }
 });

6b48c6b3d41d29d8e141baceb63c75a5f09a9f62
scorpion-jsb <tinyscorpion1101@gmail.com>
2016-01-30 20:56:53 -0500

    Add collaborator button on project tile

--- app/components/ProjectTile/ProjectTile.js
+++ app/components/ProjectTile/ProjectTile.js
@@ -3,49 +3,70 @@ import { Link } from 'react-router';
 import Paper from 'material-ui/lib/paper';
 import Avatar from 'material-ui/lib/avatar';
 import PersonIcon from 'material-ui/lib/svg-icons/social/person';
-
+import PersonAddIcon from 'material-ui/lib/svg-icons/social/person-add';
 import './ProjectTile.scss';
-const stockPhotoUrl = 'https://s3.amazonaws.com/kyper-cdn/img/User.png';
+
 export default class ProjectTile extends Component {
   constructor(props){
     super(props);
   }
   static propTypes = {
-    project: PropTypes.object,
-    onSelect: PropTypes.func
+    project: PropTypes.object.isRequired,
+    onSelect: PropTypes.func.isRequired,
+    onAddCollabClick: PropTypes.func
   };
+
   handleSelect = (e) => {
     e.preventDefault();
-    if(this.props && this.props.onSelect){
-      this.props.onSelect(this.props.project);
+    this.props.onSelect(this.props.project);
+  };
+
+  addClick = (e) => {
+    e.preventDefault();
+    if(this.props.onAddCollabClick){
+      this.props.onAddCollabClick();
     }
   };
+
   render(){
-    let collaborators;
-    if(this.props.project.collaborators){
-      collaborators = this.props.project.collaborators.map((user, i) => {
+    const { collaborators, name, owner } = this.props.project;
+    const personIconStyle = { width: '50%', height: '75%' };
+    let collaboratorsList = [];
+    if(collaborators){
+      collaboratorsList = collaborators.map((user, i) => {
         return(
-          <div key={`${this.props.project.name}-Collab-${i}`} className="ProjectTile-Collaborator">
+          <div key={`${name}-Collab-${i}`} className="ProjectTile-Collaborator">
             <Avatar
               className="ProjectTile-Collaborator-Avatar"
               src={ (user.image && user.image.url) ? user.image.url : null }
-              icon={ <PersonIcon style={{width: '50%', height: '75%'}}/> }
-              size={60}
+              icon={ <PersonIcon style={ personIconStyle }/> }
+              size={ 60 }
             />
           </div>
         );
       });
     }
+    if(this.props.onAddCollabClick){
+      collaboratorsList.push((
+        <div key={`${name}-Add-Collab`} className="ProjectTile-Collaborator" onClick={ this.addClick }>
+          <Avatar
+            className="ProjectTile-Collaborator-Avatar"
+            icon={ <PersonAddIcon style={ personIconStyle }/> }
+            size={ 60 }
+          />
+        </div>
+      ));
+    }
     return (
-      <Paper key={`Project-${this.props.project.name}`} className="ProjectTile">
+      <Paper key={`Project-${name}`} className="ProjectTile">
         <span className="ProjectTile-Name" onClick={ this.handleSelect }>
           { this.props.project.name }
         </span>
         <span className="ProjectTile-Owner">
-          { (this.props.project.owner && this.props.project.owner.username) ?  this.props.project.owner.username : 'No Owner' }
+          { (owner && owner.username) ?  owner.username : 'No Owner' }
         </span>
         <div className="ProjectTile-Collaborators">
-          { collaborators }
+          { collaboratorsList }
         </div>
       </Paper>
     );

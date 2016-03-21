a792f014646186ffb537bb40b5bca1f8059f1a77
scorpion-jsb <tinyscorpion1101@gmail.com>
2016-03-21 15:55:47 -0400

    Project tile formatting [ci skip]

--- app/components/ProjectTile/ProjectTile.js
+++ app/components/ProjectTile/ProjectTile.js
@@ -1,15 +1,16 @@
-import React, {Component, PropTypes} from 'react';
-import { Link } from 'react-router';
-import Paper from 'material-ui/lib/paper';
-import Avatar from 'material-ui/lib/avatar';
-import PersonIcon from 'material-ui/lib/svg-icons/social/person';
-import PersonAddIcon from 'material-ui/lib/svg-icons/social/person-add';
-import './ProjectTile.scss';
+import React, {Component, PropTypes} from 'react'
+import { Link } from 'react-router'
+import Paper from 'material-ui/lib/paper'
+import Avatar from 'material-ui/lib/avatar'
+import PersonIcon from 'material-ui/lib/svg-icons/social/person'
+import PersonAddIcon from 'material-ui/lib/svg-icons/social/person-add'
+import './ProjectTile.scss'
 
 export default class ProjectTile extends Component {
-  constructor(props){
-    super(props);
+  constructor (props){
+    super(props)
   }
+
   static propTypes = {
     project: PropTypes.object.isRequired,
     onSelect: PropTypes.func.isRequired,
@@ -17,65 +18,61 @@ export default class ProjectTile extends Component {
     onCollabClick: PropTypes.func
   };
 
-  handleSelect = (e) => {
-    e.preventDefault();
-    this.props.onSelect(this.props.project);
+  handleSelect = e => {
+    e.preventDefault()
+    this.props.onSelect(this.props.project)
   };
 
-  addClick = (e) => {
-    e.preventDefault();
-    if(this.props.onAddCollabClick){
-      this.props.onAddCollabClick();
-    }
+  addClick = e => {
+    e.preventDefault()
+    if (this.props.onAddCollabClick) this.props.onAddCollabClick()
   };
 
-  collaboratorClick = (collaborator) => {
-    if(this.props.onCollabClick){
-      this.props.onCollabClick(collaborator);
-    }
+  collaboratorClick = collaborator => {
+    if (this.props.onCollabClick) this.props.onCollabClick(collaborator)
   };
 
-  render(){
-    const { collaborators, name, owner } = this.props.project;
-    const personIconStyle = { width: '50%', height: '75%' };
-    let collaboratorsList = [];
-    if(collaborators){
+  render () {
+    const { collaborators, name, owner } = this.props.project
+    const personIconStyle = { width: '50%', height: '75%' }
+    let collaboratorsList = []
+    if (collaborators) {
       collaboratorsList = collaborators.map((user, i) => {
         return(
-          <div key={`${name}-Collab-${i}`} className="ProjectTile-Collaborator" onClick={ this.collaboratorClick.bind(this, user) }>
+          <div key={`${name}-Collab-${i}`} className='ProjectTile-Collaborator' onClick={ this.collaboratorClick.bind(this, user) }>
             <Avatar
-              className="ProjectTile-Collaborator-Avatar"
+              className='ProjectTile-Collaborator-Avatar'
               src={ user.avatarUrl ? user.avatarUrl : null }
-              icon={ user.avatarUrl ?  null : <Avatar style={{'fontWeight': '200'}}>{ user.username.charAt(0).toUpperCase() }</Avatar> }
+              icon={ user.avatarUrl ? null : <Avatar style={{ 'fontWeight': '200' }}> { user.username.charAt(0).toUpperCase() } </Avatar> }
               size={ 60 }
             />
           </div>
-        );
-      });
+        )
+      })
     }
-    if(this.props.onAddCollabClick){
+    if (this.props.onAddCollabClick) {
       collaboratorsList.push((
-        <div key={`${name}-Add-Collab`} className="ProjectTile-Collaborator" onClick={ this.addClick }>
+        <div key={`${name}-Add-Collab`} className='ProjectTile-Collaborator' onClick={ this.addClick }>
           <Avatar
-            className="ProjectTile-Collaborator-Avatar"
+            className='ProjectTile-Collaborator-Avatar'
             icon={ <PersonAddIcon style={ personIconStyle }/> }
             size={ 60 }
           />
         </div>
-      ));
+      ))
     }
     return (
-      <Paper key={`Project-${name}`} className="ProjectTile">
-        <span className="ProjectTile-Name" onClick={ this.handleSelect }>
+      <Paper key={`Project-${name}`} className='ProjectTile'>
+        <span className='ProjectTile-Name' onClick={ this.handleSelect }>
           { this.props.project.name }
         </span>
-        <span className="ProjectTile-Owner">
-          { (owner && owner.username) ?  owner.username : 'No Owner' }
+        <span className='ProjectTile-Owner'>
+          { (owner && owner.username) ? owner.username : 'No Owner' }
         </span>
-        <div className="ProjectTile-Collaborators">
+        <div className='ProjectTile-Collaborators'>
           { collaboratorsList }
         </div>
       </Paper>
-    );
+    )
   }
 }

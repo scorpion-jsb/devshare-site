8e919d9f27892e55a606640830588a75c5b0b1d1
scorpion-jsb <tinyscorpion1101@gmail.com>
2017-05-28 02:28:45 -0700

    app-config script improved to fix prod builds

--- build/app-config.js
+++ build/app-config.js
@@ -2,34 +2,46 @@
 const _debug = require('debug') // eslint-disable-line no-underscore-dangle
 const fs = require('fs')
 const path = require('path')
-
 const debug = _debug('app:build:config')
+const { version } = require('../package.json')
+const firebaseEnvironments = require('../config/firebaseEnvironments')
+
 const {
   TRAVIS_BRANCH,
   SENTRY_DSN,
-  GA_TRACKINGID
+  GA_TRACKINGID,
+  INT_FIREBASE_WEBAPIKEY,
+  STAGE_FIREBASE_WEBAPIKEY,
+  PROD_FIREBASE_WEBAPIKEY
 } = process.env
 
 const createConfigFile = (cb) => {
   let env = 'int'
+  let firebase = firebaseEnvironments.int
+  firebase.apiKey = INT_FIREBASE_WEBAPIKEY
 
   switch (TRAVIS_BRANCH) {
     case 'stage':
       env = 'stage'
+      firebase = firebaseEnvironments.stage
+      firebase.apiKey = STAGE_FIREBASE_WEBAPIKEY
       break
     case 'prod':
       env = 'prod'
+      firebase = firebaseEnvironments.prod
+      firebase.apiKey = PROD_FIREBASE_WEBAPIKEY
       break
     default:
       // leave int by default
   }
 
   const outputPath = path.join(__dirname, '..', 'src/config.js')
-  const fileString =
+  const fileString = `export const firebase = ${JSON.stringify(firebase, null, 2)}\n` +
+    `\nexport const version = ${JSON.stringify(version)};\n` +
     `\nexport const env = ${JSON.stringify(env)}\n` +
     `\nexport const sentryDsn = ${JSON.stringify(SENTRY_DSN || '')}\n` +
     `\nexport const gaTrackingId = ${JSON.stringify(GA_TRACKINGID || '')}\n` +
-    '\nexport default { env, gaTrackingId }\n'
+    '\nexport default { firebase, env, gaTrackingId }\n'
 
   fs.writeFile(outputPath, fileString, 'utf8', (err) => { // eslint-disable-line consistent-return
     if (err) {
--- config/firebaseEnvironments.js
+++ config/firebaseEnvironments.js
@@ -2,16 +2,22 @@ module.exports = {
   int: {
     authDomain: 'devshare-int.firebaseapp.com',
     databaseURL: 'https://devshare-int.firebaseio.com',
-    storageBucket: 'devshare-int.appspot.com'
+    projectId: 'devshare-int',
+    storageBucket: 'devshare-int.appspot.com',
+    messagingSenderId: '499842460400'
   },
   stage: {
     authDomain: 'devshare-stage.firebaseapp.com',
     databaseURL: 'https://devshare-stage.firebaseio.com',
-    storageBucket: 'devshare-stage.appspot.com'
+    projectId: 'devshare-stg',
+    storageBucket: 'devshare-stage.appspot.com',
+    messagingSenderId: '995180480938'
   },
   prod: {
     authDomain: 'devshare-1.firebaseapp.com',
     databaseURL: 'https://devshare-1.firebaseio.com',
-    storageBucket: 'devshare-1.appspot.com'
+    projectId: 'devshare-1',
+    storageBucket: 'devshare-1.appspot.com',
+    messagingSenderId: '54741256621'
   }
 }
